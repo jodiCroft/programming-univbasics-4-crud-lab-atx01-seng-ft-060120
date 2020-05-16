1b5ed8076d8f7b1c059d5d3abbcc181cf0837bf7
Jodi Croft <jodipruett@gmail.com>
2020-05-16 21:33:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["John", "Jack", "Jill"]
+  element = "Jemima"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
